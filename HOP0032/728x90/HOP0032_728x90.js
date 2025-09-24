22c554f2ab346204f81ac13f6abea6c059c15de9
DavidRiz1 <david.rizzuto@bigredgroup.com>
2025-09-24 12:55:22 +1000

    Amended 728x90 for HOP0032

--- HOP0032/728x90/HOP0032_728x90.js
+++ HOP0032/728x90/HOP0032_728x90.js
@@ -47,8 +47,8 @@ function allDocIsReady() {
 
 
 	theTimeline.add(new TweenLite.from('#logo_finder, #dropShadow', 0.5, {scale:0, ease:Power2.easeOut}));
-	theTimeline.add(new TweenLite.fromTo('#imgShine', 1.8, {x:129, y:0}, {x:-149, y:0}));
-	theTimeline.add(new TweenLite.from('#imgSparkle', 0.5, {opacity:0}), "-=0.25")
+	theTimeline.add(new TweenLite.fromTo('#imgShine', 1, {x:70, y:0}, {x:-59, y:0}));
+	theTimeline.add(new TweenLite.from('#imgSparkle', 0.5, {opacity:0}), "-=0.35")
 
 	theTimeline.add(new TweenLite.to('.f1', 0.15, {scaleX:0.05, transformOrigin: '50px 0px'}), "+=0.5")
 	theTimeline.add(new TweenLite.to('.f1', 0.13, {scaleX:-1, transformOrigin: '50px 0px'}))
