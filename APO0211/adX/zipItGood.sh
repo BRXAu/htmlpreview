caefd5ff6eed770aa1b73e5d6f051d9b62a451e9
akacyman <akacyman@gmail.com>
2026-03-16 13:40:26 +1100

    display preview

--- APO0211/adX/zipItGood.sh
+++ APO0211/adX/zipItGood.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Create a zip folder if it doesn't exist
 mkdir -p zip
 
+# Loop through all directories
 for dir in */; do
+  # Remove all main.js.LICENSE.txt files recursively inside the directory
+  find "$dir" -type f -name "main.js.LICENSE.txt" -exec rm -f {} \;
+
+  # Zip the directory excluding hidden files
   zip -r "zip/${dir%/}.zip" "$dir" -x "*/.*"
 done
